796fe6440f20ac810cea939f32b2f1ee453e05ab
jbtule <jay+code@tuley.name>
2017-08-08 11:46:37 -0500

    Adding syntactic quotes, hopefully

--- .ci/RunTests.sh
+++ .ci/RunTests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
 runTest(){
-    mono --runtime=v4.0 packages/nunit.consolerunner/3.7.0/tools/nunit3-console.exe --noresult -labels=All $@
+    mono --runtime=v4.0 packages/nunit.consolerunner/3.7.0/tools/nunit3-console.exe --noresult -labels=All "$@"
    if [ $? -ne 0 ]
    then   
      exit 1
